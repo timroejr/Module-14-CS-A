f10612416d6af3afdea694387c47920e759d3e65
Timothy Roe, Jr <timroejr@msn.com>
2015-08-26 21:57:34 -0700

    Added ChiperText Documents

--- 14.07 Challenge Program/src/FrequencyAnalysisTester.java
+++ 14.07 Challenge Program/src/FrequencyAnalysisTester.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class FrequencyAnalysisTester {
 	public static void main (String [] args) throws IOException {
 		FrequencyAnalysis fa = new FrequencyAnalysis();
-		File file = new File("/Users/timothyroejr/Desktop/Comp Science/Module 14/14.07 Challenge Program/plaintext.txt");
+		File file = new File("/Users/timothyroejr/Desktop/Comp Science/Module 14/14.07 Challenge Program/chipertext.txt");
 		fa.setupAlphabet();
 		fa.frequencyFromSubset(file);
 		fa.printFromSubsetData();
